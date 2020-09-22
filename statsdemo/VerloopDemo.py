97e999b6780e4d74123502cf7472ccf15b2e4dec
NirantK <nirant.bits@gmail.com>
2020-09-22 16:56:21 +0530

    Add MarianMT Augment

--- statsdemo/VerloopDemo.py
+++ statsdemo/VerloopDemo.py
@@ -7,7 +7,7 @@ import pandas as pd
 import streamlit as st
 import textacy
 import textacy.similarity
-import textacy.tm  
+import textacy.tm
 import textacy.vsm
 from textacy.spacier.doc_extensions import get_preview, get_tokens
 
@@ -68,6 +68,97 @@ corpus = make_corpus(df, col_name="QueryText", min_token_count=min_token_count)
 corpus
 "Docs:", corpus.n_docs, "Sentences:", corpus.n_sents, "Tokens:", corpus.n_tokens
 
+
+"## Generate Sentences Similar to Input"
+
+from transformers import MarianMTModel, MarianTokenizer
+
+
+def translate(texts, model, tokenizer, language="fr"):
+    """"""
+    # Prepare the text data into appropriate format for the model
+    template = lambda text: f"{text}" if language == "en" else f">>{language}<< {text}"
+    src_texts = [template(text) for text in texts]
+    # Tokenize the texts
+    encoded = tokenizer.prepare_seq2seq_batch(src_texts)
+
+    # Generate translation using model
+    translated = model.generate(**encoded)
+
+    # Convert the generated tokens indices back into text
+    translated_texts = tokenizer.batch_decode(translated, skip_special_tokens=True)
+
+    return translated_texts
+
+
+@st.cache
+def download_model_tokenizer_files(romance_lang: str = "ROMANCE"):
+    ROMANCE = romance_lang
+
+    target_model_name = f"Helsinki-NLP/opus-mt-en-{ROMANCE}"
+    target_tokenizer = MarianTokenizer.from_pretrained(target_model_name)
+    target_model = MarianMTModel.from_pretrained(target_model_name)
+
+    en_model_name = f"Helsinki-NLP/opus-mt-{ROMANCE}-en"
+    en_tokenizer = MarianTokenizer.from_pretrained(en_model_name)
+    en_model = MarianMTModel.from_pretrained(en_model_name)
+    return en_model, en_tokenizer, target_model, target_tokenizer
+
+
+def back_translate(texts, source_lang="en", target_lang="fr"):
+    """
+    Paraphrasing via Back Translation
+    """
+    (
+        en_model,
+        en_tokenizer,
+        target_model,
+        target_tokenizer,
+    ) = download_model_tokenizer_files(romance_lang=target_lang)
+
+    # Translate from source to target language
+    fr_texts = translate(texts, target_model, target_tokenizer, language=target_lang)
+
+    # Translate from target language back to source language
+    back_translated_texts = translate(
+        fr_texts, en_model, en_tokenizer, language=source_lang
+    )
+
+    return back_translated_texts
+
+
+from random import choice
+from typing import List
+
+download_model_tokenizer_files("ROMANCE")
+download_model_tokenizer_files("zh")
+download_model_tokenizer_files("hi")
+
+
+@st.cache
+def augment(en_texts: List[str]):
+    random_romance_lang = choice(["fr", "es", "la", "it", "pt", "ro"])
+    random_asian_lang = choice(["zh", "hi"])
+    return back_translate(
+        back_translate(en_texts, source_lang="en", target_lang="zh"),
+        source_lang="en",
+        target_lang="es",
+    )
+
+
+input_sentences = st.text_input(
+    "Enter as many sentences as you want, separated by ||"
+).split("||")
+min_freq_count = st.slider(
+    "How many tries do you want to make?",
+    value=1,
+    min_value=1,
+    max_value=5,
+)
+
+while min_freq_count > 0:
+    st.markdown(back_translate(input_sentences))
+    min_freq_count -= 1
 
 if st.checkbox("Explore Word Frequencies"):
     st.markdown("## Word Frequencies Exploration")
