9ac8b1038576b6ff32b9d009a47bcb18d5d38195
NirantK <nirant.bits@gmail.com>
2020-09-24 18:51:52 +0530

    Add sasta requirements

--- statsdemo/VerloopDemo.py
+++ statsdemo/VerloopDemo.py
@@ -13,13 +13,16 @@ import textacy.vsm
 from fastcore.utils import Path
 from textacy.spacier.doc_extensions import get_preview, get_tokens
 
-st.title("Text Analysis")
+st.title("Alfaaz: Text Exploration Tools")
+st.image(
+    image="https://verloop.io/wp-content/uploads/2020/08/cropped-VP.io-Website-Grey@2x.png"
+)
 st.markdown("Demo Client: **Abhibus**")
 preview_count = 5
 min_freq_count = 1
 
 api_url = st.text_input(
-    "Please share your Sheet **API** link here",
+    "Please share your Sheet *API* link here",
     value="https://api.steinhq.com/v1/storages/5f619d225d3cdc44fcd7d4b1",
 )
 sheet = st.text_input("Please input your sheet name here:", value="Queries150")
@@ -69,98 +72,6 @@ def make_corpus(
 corpus = make_corpus(df, col_name="QueryText", min_token_count=min_token_count)
 corpus
 "Docs:", corpus.n_docs, "Sentences:", corpus.n_sents, "Tokens:", corpus.n_tokens
-
-if st.checkbox("Generate Sentences Similar to Input", value=False):
-
-    "### This works for BANKING ONLY"
-
-    input_sentence = st.text_input(
-        "Enter sentence which you want to augment?",
-        value="When is my credit card bill due?",
-    ).split("||")
-    min_freq_count = st.slider(
-        "How many tries do you want to make?",
-        value=1,
-        min_value=1,
-        max_value=5,
-    )
-
-    intent = st.text_input("Enter a 2 word intent please", value="Due Date")
-    append_phrase = f"Sentence: {input_sentence}\nIntent: {intent}\n Paraphrase:"
-
-    prompt = (
-        """
-    Sentence :  Hello, I want to know about my card status so can you please tell me?
-    Intent : Card Status
-    Paraphrase : Hello, Can you tell me about the card status? It would be really nice if I can get this information
-    Sentence :  Hello, I wanted to know my payment due
-    Intent : Payment Amount Due
-    Paraphrase : I want information about the payment amount due, who do I contact about this?
-    Sentence :  HELLO, I want to register for a security PIN because I want to use it for online transactions.
-    Intent : e-PIN Registration
-    Paraphrase : I want to use e-pin for online transactions so I would like to know about how to register for security pin
-    Sentence :  I want to know the status of my covered card/debit card because i am going to travel.
-    Intent : Card Status
-    Paraphrase : I am going to travel. Can you tell me about eh covered card status
-    Sentence :  I would like to donate to Zakat because I want to be good.
-    Intent : Donation
-    Paraphrase : I want to donate money out of my goodness
-    Sentence :  I want to know what is my covered card outstanding
-    Intent : Payment Amount Due
-    Paraphrase : What is the outstanding balance for the covered card? I would like to know that but I am unable to find these details anywhere?
-    Sentence :  Hello, I want to know what was the last debit transaction on my Debit card because I want to know if somebody is using it.
-    Intent : Debit Transaction
-    Paraphrase : Hello, what was the last debit transaction on my card because I suspect someone else is using it.
-    Sentence :  HELLO, I FORGOT MY E-PIN SO CAN YOU RESET IT FOR ME PLEASE?
-    Intent : e-PIN Reset
-    Paraphrase : I want to reset my e-pin because I forgot.
-    Sentence :  Send me a covered card statement because I want to check my account.
-    Intent : Covered Card e-statement
-    Sentence:
-    """
-        + append_phrase
-    )
-
-    keys_path = Path("key.json")
-    try:
-        assert keys_path.exists()
-        backup_api_key = json.loads(keys_path.open("r").read())["api_key"]
-    except:
-        input_api_key = st.text_input("Enter your GPT3 key")
-
-    input_api_key = st.text_input("Enter your GPT3 key", backup_api_key)
-    engine_id = st.selectbox(
-        "Engine Id", options=["davinci", "davinci-beta", "curie", "curie-beta"]
-    )
-    import openai
-
-    openai.api_key = input_api_key
-
-    def calling_our_simulation_overlords(prompt: str, min_freq_count: int):
-        response = openai.Completion.create(
-            engine="davinci-beta",
-            prompt=prompt,
-            max_tokens=50,
-            temperature=0.9,
-            frequency_penalty=0.8,
-            n=min_freq_count,
-            stop="Sentence",
-        )
-        return response
-
-    response = calling_our_simulation_overlords(
-        prompt=prompt, min_freq_count=min_freq_count
-    )
-
-    generated_sentences = [
-        choice["text"].strip() for choice in response.to_dict()["choices"]
-    ]
-    # generated_sentences = [back_translate(s) for s in generated_sentences]
-
-    "### Output Sentence"
-    for idx, sentence in enumerate(list(set(generated_sentences))):
-        st.markdown(f"{idx+1}) {sentence}")
-        st.markdown("\n")
 
 if st.checkbox("Explore Word Frequencies"):
     st.markdown("## Word Frequencies Exploration")
@@ -268,3 +179,95 @@ if st.checkbox("Words in Sentences Explorer"):
             sent_count -= 1
         if sent_count == 0:
             break
+
+if st.checkbox("Generate Sentences Similar to Input", value=False):
+
+    "### This works for BANKING ONLY"
+
+    input_sentence = st.text_input(
+        "Enter sentence which you want to augment?",
+        value="When is my credit card bill due?",
+    ).split("||")
+    min_freq_count = st.slider(
+        "How many tries do you want to make?",
+        value=1,
+        min_value=1,
+        max_value=5,
+    )
+
+    intent = st.text_input("Enter a 2 word intent please", value="Due Date")
+    append_phrase = f"Sentence: {input_sentence}\nIntent: {intent}\n Paraphrase:"
+
+    prompt = (
+        """
+    Sentence :  Hello, I want to know about my card status so can you please tell me?
+    Intent : Card Status
+    Paraphrase : Hello, Can you tell me about the card status? It would be really nice if I can get this information
+    Sentence :  Hello, I wanted to know my payment due
+    Intent : Payment Amount Due
+    Paraphrase : I want information about the payment amount due, who do I contact about this?
+    Sentence :  HELLO, I want to register for a security PIN because I want to use it for online transactions.
+    Intent : e-PIN Registration
+    Paraphrase : I want to use e-pin for online transactions so I would like to know about how to register for security pin
+    Sentence :  I want to know the status of my covered card/debit card because i am going to travel.
+    Intent : Card Status
+    Paraphrase : I am going to travel. Can you tell me about eh covered card status
+    Sentence :  I would like to donate to Zakat because I want to be good.
+    Intent : Donation
+    Paraphrase : I want to donate money out of my goodness
+    Sentence :  I want to know what is my covered card outstanding
+    Intent : Payment Amount Due
+    Paraphrase : What is the outstanding balance for the covered card? I would like to know that but I am unable to find these details anywhere?
+    Sentence :  Hello, I want to know what was the last debit transaction on my Debit card because I want to know if somebody is using it.
+    Intent : Debit Transaction
+    Paraphrase : Hello, what was the last debit transaction on my card because I suspect someone else is using it.
+    Sentence :  HELLO, I FORGOT MY E-PIN SO CAN YOU RESET IT FOR ME PLEASE?
+    Intent : e-PIN Reset
+    Paraphrase : I want to reset my e-pin because I forgot.
+    Sentence :  Send me a covered card statement because I want to check my account.
+    Intent : Covered Card e-statement
+    Sentence:
+    """
+        + append_phrase
+    )
+
+    keys_path = Path("key.json")
+    try:
+        assert keys_path.exists()
+        backup_api_key = json.loads(keys_path.open("r").read())["api_key"]
+    except:
+        input_api_key = st.text_input("Enter your GPT3 key")
+
+    input_api_key = st.text_input("Enter your GPT3 key", backup_api_key)
+    engine_id = st.selectbox(
+        "Engine Id", options=["davinci", "davinci-beta", "curie", "curie-beta"]
+    )
+    import openai
+
+    openai.api_key = input_api_key
+
+    def calling_our_simulation_overlords(prompt: str, min_freq_count: int):
+        response = openai.Completion.create(
+            engine="davinci-beta",
+            prompt=prompt,
+            max_tokens=50,
+            temperature=0.9,
+            frequency_penalty=0.8,
+            n=min_freq_count,
+            stop="Sentence",
+        )
+        return response
+
+    response = calling_our_simulation_overlords(
+        prompt=prompt, min_freq_count=min_freq_count
+    )
+
+    generated_sentences = [
+        choice["text"].strip() for choice in response.to_dict()["choices"]
+    ]
+    # generated_sentences = [back_translate(s) for s in generated_sentences]
+
+    "### Output Sentence"
+    for idx, sentence in enumerate(list(set(generated_sentences))):
+        st.markdown(f"{idx+1}) {sentence}")
+        st.markdown("\n")
